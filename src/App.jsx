7480851e1b7f3f0be2429d770f02ca96b6ba04d5
George-Okumu <gokumu12@gmail.com>
2024-07-31 16:36:56 +0300

    Feat: using new data

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,11 @@ function App() {
   return (
     <>
 
-        <h1>Welcome to Recipe App</h1>
+        <h1 className="text-center text-2xl mt-4 font-bold text-blue-500">Welcome to Recipe App</h1>
 
-        <RecipeList recipes={recipes}/>
-
-        {/*{*/}
-        {/*    recipes.map((recipe, index) => (*/}
-        {/*    <RecipeList key={index} recipe={recipe}></RecipeList>*/}
-        {/*    ))*/}
-        {/*}*/}
-
+        <div className={"grid xl:grid-cols-4  lg:grid-cols-3 md:grid-cols-2 gap-4 mx-20 mt-20"}>
+            <RecipeList recipes={recipes}/>
+        </div>
     </>
   )
 }
